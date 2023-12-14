598f6c11c7c16c5de7d92d7a98f792663b211ea4
worldqna <worldqna21@gmail.com>
2023-12-14 21:34:22 +0530

    update

--- utils.js
+++
@@ -1,9 +0,0 @@
-function toggleSearchPopup() {
-    var searchPopup = document.getElementById("search-popup");
-    searchPopup.style.display = (searchPopup.style.display === "block") ? "none" : "block";
-}
-
-function toggleMenuPopup() {
-    var menuPopup = document.getElementById("menu-popup");
-    menuPopup.style.display = (menuPopup.style.display === "block") ? "none" : "block";
-}
